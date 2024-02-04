b65cf473a1be4cd23788b77f019569d7556356cf
Bruno Frascino <bfrascino80@gmail.com>
2024-02-04 22:31:44 +1100

    feat:Env vars, enable mock instead of fetch

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,61 +4,48 @@ import { mockBrandSyncDetails, mockCategorySynchronizationDetails } from './mock
 import { FreshSBrand, FreshSCategory } from './types';
 
 const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const isStandAlone = (process.env.REACT_APP_IS_STAND_ALONE && process.env.REACT_APP_IS_STAND_ALONE === 'true') || false;
 
 export async function getBrandSynchronizationDetails(): Promise<any> {
   try {
-    // const response = await axios.get(`${apiUrl}/synchronization/brand`);
-    // return response.data;
-    return mockBrandSyncDetails;
-    
+    if (isStandAlone) return mockBrandSyncDetails;
+
+    const response = await axios.get(`${apiUrl}/synchronization/brand`);
+    return response.data;
   } catch (error) {
-    // log.error(error);
-    log.error('Failed to get brand synchronization details');
+    log.error(`Failed to get brand synchronization details with error: ${error}`);
     return undefined;
   }
 }
 
 export async function insertBrandDetails(brand: FreshSBrand) {
-  try {
-    
-  } catch (error) {
-    
-  }
+  // try {
+  // } catch (error) {}
 }
 
 export async function updateBrandDetails(brand: FreshSBrand) {
-  try {
-    
-  } catch (error) {
-    
-  }
+  // try {
+  // } catch (error) {}
 }
 
 export async function getCategorySynchronizationDetails(): Promise<any> {
   try {
-    // const response = await axios.get(`${apiUrl}/synchronization/category`);
-    // return response.data;
-    return mockCategorySynchronizationDetails;
-    
+    if (isStandAlone) return mockCategorySynchronizationDetails;
+
+    const response = await axios.get(`${apiUrl}/synchronization/category`);
+    return response.data;
   } catch (error) {
-    // log.error(error);
-    log.error('Failed to get category synchronization details');
+    log.error(`Failed to get category synchronization details with error: ${error}`);
     return undefined;
   }
 }
 
 export async function insertCategoryDetails(category: FreshSCategory) {
-  try {
-    
-  } catch (error) {
-    
-  }
+  // try {
+  // } catch (error) {}
 }
 
 export async function updateCategoryDetails(category: FreshSCategory) {
-  try {
-    
-  } catch (error) {
-    
-  }
+  // try {
+  // } catch (error) {}
 }
