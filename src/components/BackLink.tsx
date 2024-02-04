eb5b7cd7d64e75795aeefb78654ce6674b0cd168
Bruno Frascino <bfrascino80@gmail.com>
2024-02-04 15:46:06 +1100

    feat:Back link component

---
+++ src/components/BackLink.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+
+interface BackLinkProps {
+    link: string;
+    text: string;
+}
+
+const BackLink: React.FC<BackLinkProps> = ({link, text}) => (
+    <div style={{ marginTop: '10px', marginBottom: '5px' }}>
+        <Link to={link}> <FontAwesomeIcon icon={faArrowLeft} />  {text}</Link>
+    </div>
+);
+
+export default BackLink;
