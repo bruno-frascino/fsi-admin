ebe2ef7aca85d523ead8185c5cdc1f68478ac0b6
Bruno Frascino <bfrascino80@gmail.com>
2024-02-04 15:51:13 +1100

    feat:Add PrimeReact capability

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,29 @@ import React from 'react';
 import LandingPage from './pages';
 import SyncBrandPage from './pages/sync/brand';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 import SyncCategoryPage from './pages/sync/category';
+import ErrorPage from './pages/error';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" Component={LandingPage} />
-        <Route path="/sync/brand" Component={SyncBrandPage} />
-        <Route path="/sync/category" Component={SyncCategoryPage} />
-        {/* Define other routes here if needed */}
-        {/* For example: */}
-        {/* <Route path="/about" component={AboutPage} /> */}
-        {/* <Route path="/contact" component={ContactPage} /> */}
+    <PrimeReactProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage/>} />
+          <Route path="/sync/brand" element={<SyncBrandPage/>} />
+          <Route path="/sync/category" element={<SyncCategoryPage/>} />
+          <Route path="/error" element={<ErrorPage/>} />
+          {/* Define other routes here if needed */}
+          {/* For example: */}
+          {/* <Route path="/about" component={AboutPage} /> */}
+          {/* <Route path="/contact" component={ContactPage} /> */}
 
-        {/* A catch-all route for 404 Not Found */}
-        {/* <Route path="*" component={NotFoundPage} /> */}
-      </Routes>
-    </Router>
+          {/* A catch-all route for 404 Not Found */}
+          {/* <Route path="*" component={NotFoundPage} /> */}
+        </Routes>
+      </Router>
+    </PrimeReactProvider>
   );
 }
 
