47773458c404df923460fb6c8e44a831c4d5e2c7
Bruno Frascino <bfrascino80@gmail.com>
2024-02-04 15:52:12 +1100

    wip: New fetch placeholder fns

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import log from '../logger';
-import { mockBrandSyncDetails } from './mock';
+import { mockBrandSyncDetails, mockCategorySynchronizationDetails } from './mock';
+import { FreshSBrand, FreshSCategory } from './types';
 
 const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 export async function getBrandSynchronizationDetails(): Promise<any> {
   try {
-    // const response = await axios.get(`${apiUrl}/api/synchronization/brand`);
+    // const response = await axios.get(`${apiUrl}/synchronization/brand`);
     // return response.data;
     return mockBrandSyncDetails;
     
@@ -14,5 +15,50 @@ export async function getBrandSynchronizationDetails(): Promise<any> {
     // log.error(error);
     log.error('Failed to get brand synchronization details');
     return undefined;
+  }
+}
+
+export async function insertBrandDetails(brand: FreshSBrand) {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+
+export async function updateBrandDetails(brand: FreshSBrand) {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+
+export async function getCategorySynchronizationDetails(): Promise<any> {
+  try {
+    // const response = await axios.get(`${apiUrl}/synchronization/category`);
+    // return response.data;
+    return mockCategorySynchronizationDetails;
+    
+  } catch (error) {
+    // log.error(error);
+    log.error('Failed to get category synchronization details');
+    return undefined;
+  }
+}
+
+export async function insertCategoryDetails(category: FreshSCategory) {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+
+export async function updateCategoryDetails(category: FreshSCategory) {
+  try {
+    
+  } catch (error) {
+    
   }
 }
